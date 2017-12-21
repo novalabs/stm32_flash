b7246c0a09c9bde42b27aeb83940c772fd1c0e25
Davide Rizzi <d.rizzi@novalabs.io>
2017-12-21 10:23:18 +0100

    Added STM32F407VG

--- include/stm32f4xx.hpp
+++ include/stm32f4xx.hpp
@@ -13,12 +13,16 @@
 
 #if defined(STM32F407VE)
 #define FLASH_SIZE 512
+#elif defined(STM32F407VG)
+#define FLASH_SIZE 1024
 #endif
 
 namespace core {
 namespace stm32_flash {
 #if defined(STM32F407VE)
 static const std::size_t FLASH_NUMBER_OF_PAGES = 8;
+#elif defined(STM32F407VG)
+static const std::size_t FLASH_NUMBER_OF_PAGES = 12;
 #else
 #error "Unknown flash memory map"
 #endif
@@ -28,7 +32,7 @@ FLASH_SECTOR_SIZE(
     std::size_t sector
 )
 {
-#if defined(STM32F407VE)
+#if defined(STM32F407VE) or defined(STM32F407VG)
     return sector < 4 ? 0x4000 :
 		   sector == 4 ? 0x10000 :
 		   sector < FLASH_NUMBER_OF_PAGES ? 0x20000 :
@@ -44,7 +48,7 @@ FLASH_SECTOR_OFFSET(
     std::size_t sector
 )
 {
-#if defined(STM32F407VE)
+#if defined(STM32F407VE) or defined(STM32F407VG)
 	return sector == 0 ? 0 :
 		   sector < FLASH_NUMBER_OF_PAGES ? FLASH_SECTOR_OFFSET(sector - 1) + FLASH_SECTOR_SIZE(sector):
 		   0xFFFFFFFF;
@@ -58,7 +62,7 @@ FLASH_SECTOR_ADDRESS(
     std::size_t sector
 )
 {
-#if defined(STM32F407VE)
+#if defined(STM32F407VE) or defined(STM32F407VG)
     return sector < FLASH_NUMBER_OF_PAGES ? 0x08000000 + FLASH_SECTOR_OFFSET(sector) : 0xFFFFFFFF;
 
 #else
@@ -74,7 +78,8 @@ FLASH_ADDRESS_SECTOR(
 #define X(N) (address >= FLASH_SECTOR_ADDRESS(N) && address < FLASH_SECTOR_ADDRESS(N+1)) ? N :
 #if defined(STM32F407VE)
     return X(0) X(1) X(2) X(3) X(4) X(5) X(6) X(7) 0xFFFFFFFF;
-
+#elif defined(STM32F407VG)
+    return X(0) X(1) X(2) X(3) X(4) X(5) X(6) X(7) X(8) X(9) X(10) X(11) 0xFFFFFFFF;
 #else
 #error "Unknown flash memory map"
 #endif
